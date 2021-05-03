22c3ee7ff29a99584588c123677f109e97abfe57
Jehosheba <80175330+Jehoshii@users.noreply.github.com>
2021-05-03 17:09:16 +0100

    changed html and javascript

--- script.js
+++ script.js
@@ -62,14 +62,5 @@ function weatherWatch(response) {
   imageElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
 }
-
-function collectForecast(coordinates){
-  let apiKey = `13d9fcdae5c8ed4497e6b27254968acc`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(foreshadowing);
-}
-
 let button = document.querySelector("button");
 button.addEventListener("click", collectForecast);
-
-
